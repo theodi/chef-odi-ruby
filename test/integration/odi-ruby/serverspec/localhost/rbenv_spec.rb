0f5e2904f54e30bd31265c91e19bc2ba4d30f854
James Smith <james@floppy.org.uk>
2014-07-09 14:59:53 +0100

    check we can bundle

--- test/integration/odi-ruby/serverspec/localhost/rbenv_spec.rb
+++ test/integration/odi-ruby/serverspec/localhost/rbenv_spec.rb
@@ -1,13 +1,18 @@
 require 'spec_helper'
 
-describe command('~/.rbenv/bin/rbenv -v') do
+describe command('su - vagrant -c "~/.rbenv/bin/rbenv -v"') do
   it { should return_stdout /rbenv \d.\d.\d/ }
 end
 
-describe command('~/.rbenv/shims/ruby -v') do
+describe command('su - vagrant -c "~/.rbenv/shims/ruby -v"') do
   it { should return_stdout /ruby 2.1.2/ }
 end
 
-describe command('~/.rbenv/shims/bundle -v') do
+describe command('su - vagrant -c "~/.rbenv/shims/bundle -v"') do
   it { should return_stdout /Bundler version 1.6.\d/ }
+end
+
+# Check we can actually do a bundle
+describe command('su - vagrant -c "rm -rf test; git clone https://github.com/badges/badgerbadgerbadger.git test; cd test; ~/.rbenv/shims/bundle"') do
+  it { should return_exit_status 0 }
 end
